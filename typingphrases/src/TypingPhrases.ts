2072f339fe628b96830a44b735ca0c43fa1659b6
raulmar <raulhdz40@gmail.com>
2020-12-27 17:03:10 -0600

    converted to TS

---
+++ typingphrases/src/TypingPhrases.ts
@@ -0,0 +1,63 @@
+const PHRASES = [
+  'Changing Layout...',
+  'Choosing color palette...', 
+  'Improving performace...', 
+  'Updating projects...'
+]
+
+let i = 0
+
+class TypingPhrases {
+  phraseContainer: HTMLElement
+  input: string[]
+  phrases: string[]
+  currentPhrase: string
+
+  constructor(input: string[]) {
+    this.phraseContainer = document.querySelector('.phrase')
+    this.input = input
+    window.addEventListener('load', this.initPhrases())
+  }
+
+  initPhrases(): any {
+    this.phrases = [...this.input]
+    
+    setInterval(() => {
+      this.renderPhrase()
+    },0)
+  }
+  
+  renderPhrase() {
+    if(this.currentPhrase) {
+      return
+    }
+    const phrase = this.getPhrase()
+    this.renderLetters(phrase)
+    this.currentPhrase = phrase
+    
+    setTimeout(() => {
+      this.currentPhrase = null
+      this.phraseContainer.innerHTML = ''
+      i = 0
+    }, 3000)
+  }
+
+  getPhrase() {
+    if(this.phrases.length === 0) {
+      this.initPhrases()
+    }
+    return this.phrases.pop()
+  }
+
+  renderLetters(phrase) {
+    if(i < phrase.length) {
+      // debugger
+      this.phraseContainer.innerHTML += phrase.charAt(i)
+      i++
+      setTimeout(() => {this.renderLetters(phrase)},100)
+    }
+    return
+  }
+}
+
+const startTyping = new TypingPhrases(PHRASES)
